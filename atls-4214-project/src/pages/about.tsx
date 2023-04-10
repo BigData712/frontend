db4c55c0c7456beb49a73e44e38713daad873f86
Noah <noahxn@gmail.com>
2023-04-09 20:36:43 -0600

    implementing about page

--- atls-4214-project/src/pages/about.tsx
+++ atls-4214-project/src/pages/about.tsx
@@ -1,4 +1,5 @@
-import { Paper, Typography } from '@mui/material';
+import { PausePresentationRounded } from '@mui/icons-material';
+import { Paper, Typography,Grid } from '@mui/material';
 import React from 'react'
 
 
@@ -7,13 +8,15 @@ export default function AboutPage() {
         <>
             <div
                 style={{
-                    padding: 15
+                    padding: 15,
+                    maxWidth: '80vw',
+                    margin: 'auto'
                 }}
             >
                 <Paper 
                     elevation={3}
                     style={{
-                        maxWidth: '67vw',
+                        // maxWidth: '80vw',
                         margin: 'auto',
                         padding: '20px'
                     }}
@@ -22,9 +25,67 @@ export default function AboutPage() {
                         variant='h2'
                         textAlign='center'
                     >
-                        About
+                        About Crime Crunch
                     </Typography>
                 </Paper>
+                <br></br>
+                <Grid container spacing={2}>
+                    <Grid item xs={6} md={7}>
+                        <Paper
+                            elevation={3}
+                            style={{
+                                // maxWidth: '67vw',
+                                margin: 'auto',
+                                padding: '20px',
+                                height: '250px'
+                            }}
+                        >
+                            <Typography
+                                variant='h3'
+                                textAlign='left'
+                            >
+                                What is Crime Crunch?
+                            </Typography>
+                            <br></br>
+                            <Typography
+                                variant='body1'
+                                textAlign='left'
+                            >
+                                Crime Crunch is an app designed to help Coloradans learn more about the different kinds of crimes
+                                occurring across the state.  Utilizing data from the <a href="https://cde.ucr.cjis.gov/LATEST/webapp/#/pages/home">FBI Crime Data Explorer</a>,
+                                <div style={{color: 'blue'}}> <b> wewo</b></div>
+                            </Typography>
+                            
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6} md={5}>
+                        <Paper
+                            elevation={3}
+                            style={{
+                                // maxWidth: '67vw',
+                                margin: 'auto',
+                                padding: '20px',
+                                height: '250px'
+                            }}
+                        >
+                            <Typography
+                                variant='h4'
+                                textAlign='left'
+                            >
+                                Why did we make Crime Crunch?
+                            </Typography>
+                            <br></br>
+                            <Typography
+                                variant='body1'
+                                textAlign='left'
+                            >
+                                With reports of crimes on the rise, particularly in the cases of hate crimes or those involving violence, 
+                                it is important for residents to be aware and informed on what is happening around them. 
+                            </Typography>
+                            
+                        </Paper>
+                    </Grid>
+                </Grid>
             </div>
         </>
     );
