676f5213cc50cd3a9617944e6e958668f3355867
Jaryd Meek <jarydmeek@gmail.com>
2023-04-14 20:06:34 -0600

    fixing lint issue

--- atls-4214-project/src/pages/index.tsx
+++ atls-4214-project/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Box, Button, Container, Divider, Paper, Typography } from '@mui/material'
+import { Box, Stack, Button, Container, Divider, Paper, Typography } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
